51609097ddfb8f6b71caa1dbe090a48fbc9e6874
root <laupadgon@alum.us.es>
2020-03-22 13:38:35 +0100

    check authenticated

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -4,31 +4,35 @@ var vault = require('../provider/vault');
 
 exports.ensureAuth = function(req, res, next){
   if(!req.headers.authorization){
-    console.log('No hay cabecera de autenticación')
-    return res.status(403).send({message: 'No hay cabecera de autenticación'})
+    console.log('No hay cabecera de autenticación');
+    return res.status(403).send({message: 'No hay cabecera de autenticación'});
   }
 
-  vault.read('secret/tokenJWT')
+  return vault.read('secret/tokenJWT')
     .then((res) => {
-      var secret = res.data.secret;
-      var token = req.headers.authorization.replace(/['"]+/g, '');
-
-      try{
-        var payload = jwt.decode(token, secret);
-        if(payload.expireTime <= moment().unix()){
-          console.log('Token ha expirado')
+      let secret = res.data.secret;
+      let token = req.headers.authorization.replace(/['"]+/g, '');
+      return jwt.decode(token,secret);
+    }).then((payload) => {
+      console.log("payload: " + payload);
+      if(payload.expireTime <= moment().unix()){
+          console.log('Token ha expirado');
           return res.status(401).send({message: 'Token ha expirado'});
-        }
-      }catch(ex){
-        console.log(ex);
-        console.log('Token no válido')
-        return res.status(404).send({message: 'Token no válido'});
+      }else{
+	return payload;
       }
+    }).catch((ex) => {
+      console.log("Error: " + ex);
+      console.log('Token no válido');
+      return res.status(404).send({message: 'Token no válido'});
+    }).then((p) => {
+	console.log("payload: " + p.expireTime);
+	req.user = p;
+	next();
+    }).catch((err) => {
+	console.log("Error: " + err);
+	next();
+    });
 
-      req.user = payload;
-
-      next();
-
-    })
-    .catch((err) => next());  
+    next();
 };
