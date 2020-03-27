aea7ceb6ca5dd4981d24baa30e44bfae1ae1bece
Fernando López <ferlopezcarr@gmail.com>
2020-03-27 09:15:11 +0100

    Sacadas funciones del on load

--- public/js/deleteUser.js
+++ public/js/deleteUser.js
@@ -1,104 +1,99 @@
 "use strict"
-$(function() {
 
-    //
-	// $('#element').donetyping(callback[, timeout=1000])
-	// Fires callback when a user has finished typing. This is determined by the time elapsed
-	// since the last keystroke and timeout parameter or the blur event--whichever comes first.
-	//   @callback: function to be called when even triggers
-	//   @timeout:  (default=1000) timeout, in ms, to to wait before triggering event if not
-	//	              caused by blur.
-	// Requires jQuery 1.7+
-	//
-    $.fn.extend({
-        donetyping: function(callback,timeout){
-            timeout = timeout || 1.5e3; // 1 second default timeout
-            var timeoutReference,
-                doneTyping = function(el){
-                    if (!timeoutReference) return;
-                    timeoutReference = null;
-                    callback.call(el);
-                };
-            return this.each(function(i,el){
-                var $el = $(el);
-                // Chrome Fix (Use keyup over keypress to detect backspace)
-                // thank you @palerdot
-                $el.is(':input') && $el.on('keyup keypress paste',function(e){
-                    // This catches the backspace button in chrome, but also prevents
-                    // the event from triggering too preemptively. Without this line,
-                    // using tab/shift+tab will make the focused element fire the callback.
-                    if (e.type=='keyup' && e.keyCode!=8) return;
-                    
-                    // Check if timeout has been set. If it has, "reset" the clock and
-                    // start over again.
-                    if (timeoutReference) clearTimeout(timeoutReference);
-                    timeoutReference = setTimeout(function(){
-                        // if we made it here, our timeout has elapsed. Fire the
-                        // callback
-                        doneTyping(el);
-                    }, timeout);
-                }).on('blur',function(){
-                    // If we can, fire the event since we're leaving the field
+//
+// $('#element').donetyping(callback[, timeout=1000])
+// Fires callback when a user has finished typing. This is determined by the time elapsed
+// since the last keystroke and timeout parameter or the blur event--whichever comes first.
+//   @callback: function to be called when even triggers
+//   @timeout:  (default=1000) timeout, in ms, to to wait before triggering event if not
+//	              caused by blur.
+// Requires jQuery 1.7+
+//
+$.fn.extend({
+    donetyping: function(callback,timeout){
+        timeout = timeout || 1.5e3; // 1 second default timeout
+        var timeoutReference,
+            doneTyping = function(el){
+                if (!timeoutReference) return;
+                timeoutReference = null;
+                callback.call(el);
+            };
+        return this.each(function(i,el){
+            var $el = $(el);
+            // Chrome Fix (Use keyup over keypress to detect backspace)
+            // thank you @palerdot
+            $el.is(':input') && $el.on('keyup keypress paste',function(e){
+                // This catches the backspace button in chrome, but also prevents
+                // the event from triggering too preemptively. Without this line,
+                // using tab/shift+tab will make the focused element fire the callback.
+                if (e.type=='keyup' && e.keyCode!=8) return;
+                
+                // Check if timeout has been set. If it has, "reset" the clock and
+                // start over again.
+                if (timeoutReference) clearTimeout(timeoutReference);
+                timeoutReference = setTimeout(function(){
+                    // if we made it here, our timeout has elapsed. Fire the
+                    // callback
                     doneTyping(el);
-                });
+                }, timeout);
+            }).on('blur',function(){
+                // If we can, fire the event since we're leaving the field
+                doneTyping(el);
+            });
+        });
+    }
+});
+
+//Delete user form
+function deleteUser() {
+    
+    let emailOk = parser.parse('#inputEmail', parser.parseEmail);
+    console.log(emailOk);
+
+    if(emailOk) {
+        let user_id = $("#deleteUserForm").attr("data-user-id");
+        let user_email = $("#deleteUserForm").attr("data-user-email");
+
+        if($('#inputEmail').val() !== user_email) {
+            parser.displayMessageError('#inputEmail', "El email introducido debe coincidir con el del usuario a dar de baja");
+        }
+        else {
+            user_email = "";
+            $("#deleteUserForm").attr("data-user-email", "");
+            $('#inputEmail').val("");
+
+            $.ajax("https://www.app.losuratech.com/authentication/user/"+user_id,{
+                type: 'DELETE',
+                headers: { Authorization: localStorage.getItem("token") },
+                data: {id: user_id}
+            })
+            .done(function(data) {
+                $("#tr_u_"+user_id).remove();
+
+                user_id = "";
+                $("#deleteUserForm").attr("data-user-id", "");
+            })
+            .fail(function() {
+                parser.displayMessageError("#inputEmail", error.message);
             });
         }
-    });
-
-    //Delete user form
-    function deleteUser() {
-        
-        let emailOk = parser.parse('#inputEmail', parser.parseEmail);
-        console.log(emailOk);
-
-        if(emailOk) {
-            alert("Email correcto");
-            let user_id = $("#deleteUserForm").attr("data-user-id");
-            let user_email = $("#deleteUserForm").attr("data-user-email");
-
-            if($('#inputEmail').val() !== user_email) {
-                parser.displayMessageError('#inputEmail', "El email introducido debe coincidir con el del usuario a dar de baja");
-            }
-            else {
-                user_email = "";
-                $("#deleteUserForm").attr("data-user-email", "");
-                $('#inputEmail').val("");
-
-                $.ajax("https://www.app.losuratech.com/authentication/user/"+user_id,{
-                    type: 'DELETE',
-                    headers: { Authorization: localStorage.getItem("token") },
-                    data: {id: user_id}
-                })
-                .done(function(data) {
-                    alert("Borrando fila");
-                    $("#tr_u_"+user_id).remove();
-
-                    user_id = "";
-                    $("#deleteUserForm").attr("data-user-id", "");
-                })
-                .fail(function() {
-                    parser.displayMessageError("#inputEmail", error.message);
-                });
-            }
-        }
     }
+}
 
-    //Parse email
-    $('#inputEmail').donetyping(function() {
-        parser.parse('#inputEmail', parser.parseEmail);
-    });
-    $('#inputEmail').change(function() {
-        parser.parse('#inputEmail', parser.parseEmail)
-    });
+//Parse email
+$('#inputEmail').donetyping(function() {
+    parser.parse('#inputEmail', parser.parseEmail);
+});
+$('#inputEmail').change(function() {
+    parser.parse('#inputEmail', parser.parseEmail)
+});
 
-    $("#deleteYes").click(function(e) {
-        e.preventDefault();
+$("#deleteYes").click(function(e) {
+    e.preventDefault();
 
-        deleteUser();
+    deleteUser();
 
-        return false;
-    });
-
+    return false;
 });
 
 function loadUserTable(users) {
